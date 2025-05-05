81335e754525172c64b6d43c25ea01beef716237
truongthanhtam7701 <truongthanhtam7701@gmail.com>
2025-05-05 20:53:38 +0700

    commit 2

---
+++ demo/src/main/java/thanh/tam/truong/demo/DemoController.java
@@ -0,0 +1,14 @@
+package thanh.tam.truong.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/")
+	public String test ()
+	{
+		return "vvv";
+	}
+}
