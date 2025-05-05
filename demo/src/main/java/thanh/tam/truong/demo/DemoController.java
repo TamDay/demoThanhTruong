cc485b5fb15a4a9a87e728e43c2bb62111b24fa9
truongthanhtam7701 <truongthanhtam7701@gmail.com>
2025-05-05 21:06:47 +0700

    test2

--- demo/src/main/java/thanh/tam/truong/demo/DemoController.java
+++ demo/src/main/java/thanh/tam/truong/demo/DemoController.java
@@ -1,6 +1,8 @@
 package thanh.tam.truong.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class DemoController {
 	public String test ()
 	{
 		return "vvv";
+	}
+	@PostMapping("/addsv")
+	public String test1 (@RequestParam String name)
+	{
+		return name;
 	}
 }
